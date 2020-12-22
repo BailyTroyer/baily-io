99482a28d1a200c2e34149180e2725a75a6c7a81
Baily Troyer <bailytroyer@gmail.com>
2020-12-21 21:00:00 -0500

    Add Support for Linting, Testing & TS (#2)
    * Adding ESLint & Typescript support
    
    * Adding Jest & sample snapshot test
    
    * removing unused gatsby configs

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -5,14 +5,20 @@
  * See: https://www.gatsbyjs.com/docs/use-static-query/
  */
 
-import React from "react"
-import PropTypes from "prop-types"
+import React, { FC, ReactNode } from "react"
+
 import { useStaticQuery, graphql } from "gatsby"
 
-import Header from "./header"
+import { Header } from "./Header"
 import "./layout.css"
 
-const Layout = ({ children }) => {
+interface Props {
+  children: ReactNode
+}
+
+const Layout: FC<Props> = (props: Props) => {
+  const { children } = props
+
   const data = useStaticQuery(graphql`
     query SiteTitleQuery {
       site {
@@ -34,9 +40,11 @@ const Layout = ({ children }) => {
         }}
       >
         <main>{children}</main>
-        <footer style={{
-          marginTop: `2rem`
-        }}>
+        <footer
+          style={{
+            marginTop: `2rem`,
+          }}
+        >
           © {new Date().getFullYear()}, Built with
           {` `}
           <a href="https://www.gatsbyjs.com">Gatsby</a>
@@ -44,10 +52,6 @@ const Layout = ({ children }) => {
       </div>
     </>
   )
-}
-
-Layout.propTypes = {
-  children: PropTypes.node.isRequired,
 }
 
 export default Layout
