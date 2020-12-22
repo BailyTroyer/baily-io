db15dcebb342890f9977cd0552ee47ef210c102c
BailyTroyer <bailytroyer@gmail.com>
2020-12-22 12:30:54 -0500

    Adding Tailwind Deps & Example

--- gatsby-config.js
+++ gatsby-config.js
@@ -27,6 +27,16 @@ module.exports = {
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
     },
+    {
+      resolve: "gatsby-plugin-postcss",
+      options: {
+        postCssPlugins: [require("tailwindcss")("./tailwind.config.js")],
+      },
+    },
+    {
+      resolve: "gatsby-plugin-emotion",
+      options: {},
+    },
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
